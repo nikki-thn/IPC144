f61a0515f2cd741822dc4522916ef61793cb09ba
nikki-thn <ntruong4@myseneca.ca>
2017-06-29 00:20:16 -0400

    Update 144_ms1.c

--- 144_ms1.c
+++ 144_ms1.c
@@ -15,7 +15,6 @@ int getIntLimited(int lowerLimit, int upperLimit);
 double getDoubleLimited(double lowerLimit, double upperLimit);
 
 
-
 // user interface tools function PROTOTYPES:
 
 
@@ -37,18 +36,14 @@ void welcome(void) {
 void printTitle(void) {
 
 	printf("Row |SKU| Name               | Price  |Taxed| Qty | Min | Total      |Atn\n");
-	printf("----+---+--------------------+--------+-----+-----+-----+------------|---");
+	printf("----+---+--------------------+--------+-----+-----+-----+------------|---\n");
 }
 
 void printFooter(double gTotal) {
 	printf("--------------------------------------------------------+----------------\n");
-	printf("                                           Grand Total: | %12.2lf", gTotal);
-}
-
-void flushKeyboard(void) {
-
-	while (getchar() != '\n') 
-		;
+	if (gTotal > 0) {
+		printf("                                           Grand Total: | %12.2lf\n", gTotal);
+	}
 }
 
 void pause(void) {
@@ -56,24 +51,151 @@ void pause(void) {
 	flushKeyboard();
 }
 
-int getInt(void) {
-	int value;
-	char newLine = 'x';
+void flushKeyboard(void) {
 
-		printf("Enter an integer: ");
-		scanf("%d%c", &value, &newLine);
-
-		while (newLine != '\n'){
-			printf("Invalid interger, please try again: ");
-			scanf("%d%c", &value, &newLine);
-	}
-	printf("%d\n", value);
+	while (getchar() != '\n')
+		;
 }
 
-int main() {
+int getInt (void) {
 
-	getInt();
+	int value;
+	int errorCheck;
+	char checkNewLine;
+	int done = 1;
 
+
+	while (done == 1) {
+
+	errorCheck = scanf("%d%c", &value, &checkNewLine);
+
+		if (checkNewLine != '\n' || errorCheck == 0) {
+			printf("Invalid interger, please try again: ");
+		}
+		else {
+			done = 0;
+		}
+	}
+
+	return value;
+}
+
+int getIntLimited(int lowerLimit, int upperLimit) {
+	int limitCheck;
+	int done = 1;
+
+	while (done == 1) {
+
+		scanf("%d", &limitCheck);
+
+		if (limitCheck > upperLimit || limitCheck < lowerLimit) {
+			printf("Invalid value, %d < value < %d", lowerLimit, upperLimit);
+		}
+		else {
+			done = 0;
+		}
+
+	}
+
+	return limitCheck;
+}
+
+double getDouble (void) {
+
+	double value;
+	double errorCheck;
+	char checkNewLine;
+	int done = 1;
+
+
+	while (done == 1) {
+
+		errorCheck = scanf("%lf%c", &value, &checkNewLine);
+
+		if (checkNewLine != '\n' || value == 0) {
+			printf("Invalid interger, please try again: ");
+		}
+		else {
+			done = 0;
+		}
+	}
+
+	return value;
+}
+
+double getDoubleLimited (double lowerLimit, double upperLimit) {
+
+	double limitCheck;
+	int done = 1;
+
+	while (done == 1) {
+
+		scanf("%lf", &limitCheck);
+
+		if (limitCheck > upperLimit || limitCheck < lowerLimit) {
+			printf("Invalid value, %d < value < %d", lowerLimit, upperLimit);
+		}
+		else {
+			done = 0;
+		}
+
+	}
+
+	return limitCheck;
+}
+
+int main(void) {
+	int iVal;
+	double dVal;
+	welcome();
+
+	// uncommnet each tester as you complete the develpment of that section:
+
+	// testing printTitle() 
+	printf("listing header and footer with grand total:\n");
+	printTitle();
+	//---------------------------------------------------------------
+
+
+	// Testing printFooter()  
+
+	printFooter(1234.5678);
+	printf("listing header and footer without grand total:\n");
+	printTitle();
+	printFooter(-1);
+	//---------------------------------------------------------------
+
+	// Testing Pause() and flushKeyboard() 
+	pause();
+	//---------------------------------------------------------------
+
+
+	// Testing getInt() 
+	printf("Enter an integer: ");
+	iVal = getInt();
+	printf("You entered: %d\n", iVal);
+	//---------------------------------------------------------------
+
+
+	// Testing Get Int Limited 
+	printf("Enter an integer between 10 and 20: ");
+	iVal = getIntLimited(10, 20);
+	printf("Your entered %d\n", iVal);
+	//---------------------------------------------------------------
+
+
+	// Testing Get Double 
+	printf("Enter a floating point number: ");
+	dVal = getDouble();
+	printf("You entered: %0.2lf\n", dVal);
+	//---------------------------------------------------------------
+
+
+	// Tesing Get Double Limited 
+	printf("Enter a floating point number between 10.00 and 20.00: ");
+	dVal = getDoubleLimited(10.0, 20.0);
+	printf("You entered: %0.2lf\n", dVal);
+	//---------------------------------------------------------------
 
 
 	return 0;
