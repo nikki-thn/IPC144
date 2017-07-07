4b4661324503a5e64b37d57fcdd561b6a5fa14c3
nikki-thn <ntruong4@myseneca.ca>
2017-07-07 14:55:47 -0400

    Update lab7_athome.c

--- lab7_athome.c
+++ lab7_athome.c
@@ -1,52 +1,58 @@
 /*
-Name:
-Student number:
-Workshop:
-Section:
+Name: Nikki Truong
+Student number: 112 314 174
+IPC 144 - Workshop 7 - At home
+Section: H
 */
+
+//This program will store user contacts number and exact it into format (area) - prefix - number 
 
 #include <stdio.h>
 
-#define SIZE 3
 
-//Function Prototypes go here
+#define SIZE 3 //Maximum number storage
 
-void decompose(const long long phoneNum[], int *areaCode, int*prefix, int *lineNum);
+//Function prototype
+void decompose(long long phoneNum, int *areaCode, int*prefix, int *lineNum);
 int isValid(const long long phoneNum);
 
-//Function Definitions go here
-void decompose(const long long phoneNum, int *areaCode, int*prefix, int *lineNum) {
+//Function declaration
+//This funtion will extract user input phone number into area code, prefix and 4 last digit number
+void decompose(long long phoneNum, int *areaCode, int*prefix, int *lineNum) {
 
-	long long temp;
+	int temp;
 
-	temp = (phoneNum % 10000000); // Exact the 
-	*lineNum = temp % 10000; //9951
-	*prefix = (temp - *lineNum) / 10000; // 501
-	*areaCode = (phoneNum - temp) / 10000000; //306
+	temp = (phoneNum % 10000000); //This line of code extract last 7 digits
+	*lineNum = temp % 10000; //This line gets the last 4 digits
+	*prefix = (temp - *lineNum) / 10000; //This codes gets the prefix digits
+	*areaCode = (phoneNum - temp) / 10000000; //This exacts the area code
 
 }
 
+//This function will check each area code and prefix if they are valid
+/*Function recieves a phone number from caller, call decompose funtion to break it up and check
+the required components*/
 int isValid(const long long phoneTemp) {
 
 	int areaCodeCheck;
 	int prefixCheck;
 	int lineNumCheck;
 
-		decompose(phoneTemp, &areaCodeCheck, &prefixCheck, &lineNumCheck);
+	decompose(phoneTemp, &areaCodeCheck, &prefixCheck, &lineNumCheck);
 
-		if ((areaCodeCheck == 416 || areaCodeCheck == 647 || areaCodeCheck == 905) && (prefixCheck > 100 && prefixCheck < 999)) {
-			return 1;
-		}
+	//Check conditions
+	if ((areaCodeCheck == 416 || areaCodeCheck == 647 || areaCodeCheck == 905) && (prefixCheck > 100 && prefixCheck < 999)) {
+		return 1;
+	}
 
-		else {
-
-			printf("ERROR!!! Phone Number is not Valid\n\n");	
-			return 0;
-
-		}
+	else {
+		//If number is not valid, print error message
+		printf("ERROR!!! Phone Number is not Valid\n");
+		
+		return 0;
+	}
 
 }
-
 
 
 /* main program */
@@ -54,11 +60,11 @@ int main(void) {
 
 	int option;
 	int count = 0;
-	long long phoneTemp;
 	long long phoneNum[SIZE] = { 0LL };
-	long long areaCode;
-	long long prefix;
-	long long lineNum;
+	long long phoneTemp = { 0LL };
+	int areaCode;
+	int prefix;
+	int lineNum;
 	int valid;
 	int i = 0;
 
@@ -82,9 +88,9 @@ int main(void) {
 		case 1: // Display the Phone List
 
 			printf("Phone Numbers\n");
-			printf("==============\n\n");
-			// Display each number in decomposed form
+			printf("==============\n");
 
+			// Display each number in decomposed form
 			for (i = 0; i < count; i++) {
 
 				decompose(phoneNum[i], &areaCode, &prefix, &lineNum);
@@ -101,31 +107,39 @@ int main(void) {
 
 		case 2:	// Add a Phone Number
 
+				//This will check the counter to make sure there are space in array for input 
+				//Everytime user input, increase the counter by one
 			if (count < SIZE) {
 
 				printf("Add a Number\n");
 				printf("============\n");
-				scanf("%lld", &phoneTemp);
-				valid = isValid(phoneTemp);
+				scanf("%lld", &phoneNum[count]);
+				valid = isValid(phoneTemp); //Here function isValid is called to check the entered number
 
-
-
+											//When the phone number is valid, store it to the array
 				if (valid == 1) {
 					phoneNum[count] = phoneTemp;
 					count++;
-					printf("\n");
+					
 				}
-
+				printf("\n");
 
 			}
+
+			//If array is full, return an error message
 			else if (count >= SIZE) {
-				printf("ERROR!!! Phone Number List is Full\n\n");
+				printf("Add a Number\n");
+				printf("============\n");
+				printf("ERROR!!! Phone Number List is Full\n");
 			}
 
 			break;
 
 		default:
-			printf("ERROR!!!: Incorrect Option: Try again\n\n");
+
+			//When user input non of the valid option
+			printf("Invalid menu option\n");
+
 			break;
 
 		}
