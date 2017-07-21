41915e9b7e6e57ac0c07f2b18fc8f0b1e8026a13
nikki-thn <ntruong4@myseneca.ca>
2017-07-21 19:39:26 -0400

    Update lab9_inlab.c

--- lab9_inlab.c
+++ lab9_inlab.c
@@ -1,16 +1,17 @@
 /*
 Name: Nikki Truong
 Student number: 112 314 174
-IPC 144 - Workshop 8 - At home
+IPC 144 - Workshop 9 - In lab
 Section: H
 */
 
-/*This program will perform a book inventory, in which it let user add data, display inventory, search for an item
-and price check and search the item by isbn (at-home) */
+/*This program will open a file, scan in information, display them and calculate the total capital
+of the bookstore*/
 
 #include<stdio.h>
 
 #define MAX_TITLE_SIZE 21
+
 
 struct Book {
 
@@ -21,47 +22,30 @@ struct Book {
 	double _price;
 };
 
+//Functions prototypes go here
+
 void menu();
 int readRecord(FILE *fp, struct Book *b2read);
-
 void displayInventory(const char filename[]);
 float calculateCapital(const char filename[]);
 
-float calculateCapital(const char filename[]) {
-
-struct Book book;
-float total_capital = 0;
-
-int rv = 0;
-
-FILE *fp = NULL;
-
-fp = fopen(filename, "r");
-
-rv = readRecord(fp, &book);
 
 
-rewind(fp);
+//Menu function print out the menu
+void menu() {
 
-
-if (rv == 5) {
-
-fp = fopen(filename, "r");
-
-while (fscanf(fp, "%d;%lf;%d;%d; %20[^\n]", &book._isbn, &book._price, &book._year, &book._qty, book._title) != EOF) {
-total_capital = total_capital + book._qty * book._price;
+	printf("Please select from the following options:\n");
+	printf("1) Display the inventory.\n");
+	printf("2) Add a book to the inventory.\n");
+	printf("3) Check price.\n");
+	printf("4) Calculate total capital of the store.\n");
+	printf("0) Exit.\n\n");
 
 }
 
 
-fclose(fp);
-}
-
-printf("The total capital is: $%.2lf.\n", total_capital);
-
-return total_capital;
-
-}
+/*This function will scan through the files and make sure there are 5 sucessful scan with given format
+and pass the number of successful scan to caller*/
 
 int readRecord(FILE *fp, struct Book *b2read) {
 
@@ -69,6 +53,7 @@ int readRecord(FILE *fp, struct Book *b2read) {
 
 	int rv = 0;
 
+	//If the file is not empty, scan the data
 	if (fp != NULL) {
 
 		rv = fscanf(fp, "%d;%lf;%d;%d; %20[^\n]", &book._isbn, &book._price, &book._year, &book._qty, book._title);
@@ -76,11 +61,12 @@ int readRecord(FILE *fp, struct Book *b2read) {
 		fclose(fp);
 	}
 
-	//printf("%d", rv);
 	return rv;
 
 }
 
+
+//This function will display the inventory when readRecoed function return 5 sucessful scans
 void displayInventory(const char filename[]) {
 
 	struct Book book = { { 0 } };
@@ -93,11 +79,9 @@ void displayInventory(const char filename[]) {
 
 	rv = readRecord(fp, &book);
 
-
-	//printf("%d", rv);
-
 	rewind(fp);
 
+	//If condition was met, display entries
 	if (rv == 5) {
 		fp = fopen(filename, "r");
 
@@ -110,6 +94,7 @@ void displayInventory(const char filename[]) {
 			printf("%-10.0d%-20s%-5d$%-8.2f%-8d\n", book._isbn, book._title, book._year, book._price, book._qty);
 
 		}
+		
 		printf("===================================================\n\n");
 
 		fclose(fp);
@@ -117,6 +102,43 @@ void displayInventory(const char filename[]) {
 
 
 }
+
+
+//This function will calculate the total capital of current inventory
+float calculateCapital(const char filename[]) {
+
+	struct Book book;
+	float total_capital = 0;
+
+	int rv = 0;
+
+	FILE *fp = NULL;
+
+	fp = fopen(filename, "r");
+
+	rv = readRecord(fp, &book);
+
+	rewind(fp);
+
+	//Again, when readRecord is good, perform the calculation
+	if (rv == 5) {
+
+		fp = fopen(filename, "r");
+
+		while (fscanf(fp, "%d;%lf;%d;%d; %20[^\n]", &book._isbn, &book._price, &book._year, &book._qty, book._title) != EOF) {
+			total_capital = total_capital + book._qty * book._price;
+
+		}
+
+		fclose(fp);
+	}
+
+	printf("The total capital is: $%.2lf.\n", total_capital);
+
+	return total_capital;
+
+}
+
 
 int main() {
 
@@ -146,27 +168,17 @@ int main() {
 			displayInventory(filename);
 			break;
 
-		case 4: //Calculate capital
-				calculateCapital(filename);
+		case 4: //Calculate total capital
+			calculateCapital(filename);
 			break;
 
 		default: //prompt error message
 			printf("Invalid input, try again:\n");
 			break;
+
 		}
 	}
 
 	return 0;
 }
 
-//Menu function print out the menu
-void menu() {
-
-	printf("Please select from the following options:\n");
-	printf("1) Display the inventory.\n");
-	printf("2) Add a book to the inventory.\n");
-	printf("3) Check price.\n");
-	printf("4) Calculate total capital of the store.\n");
-	printf("0) Exit.\n\n");
-
-}
